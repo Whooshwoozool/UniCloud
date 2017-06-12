08d2dc596e3cf25defe766ada3929d6858bc5eeb
whooshwoozool <nicefdsa@gmail.com>
2017-06-13 00:31:38 +0300

    some minor fixes

--- client/app/app.js
+++ client/app/app.js
@@ -1,10 +1,28 @@
-angular.module('myApp', ['toastr', 'ngAnimate', 'ui.bootstrap', 'ngFileUpload']);
+angular.module('myApp', ['toastr', 'ngAnimate', 'ui.bootstrap', 'ngFileUpload', 'ngRoute']);
 
-require('./fileslist/list.controller.js');
-require('./fileslist/filemanager.service.js');
+angular
+        .module('myApp')
+        .config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider){
+        	$routeProvider.when('/list', {
+    			templateUrl: './app/templates/list.html'
+    		});
+    		$routeProvider.when('/login', {
+    			templateUrl: './app/templates/login.html'
+    		});
+    		$routeProvider.when('/list/:foldername', {
+    			templateUrl: './app/templates/folder.html'
+    		});
+        }]);
+
+require('./fileslist/list.controller');
+require('./fileslist/filemanager.service');
 require('./widget/widget');
 require('./notificator/notifications');
 require('./fileslist/modal/modal');
 require('./fileslist/modal/uploadfile.service');
 require('./fileslist/modal/editfolder.service');
-require('./fileslist/modal/createfolder.service.js');
+require('./fileslist/modal/createfolder.service');
+require('./fileslist/modal/replacefile.service');
+require('./defaultCtrl/default.controller');
+require('./login/authentification.service');
+require('./login/login.controller');
